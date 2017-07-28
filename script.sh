159989dd484040150efb54626530b6d659c7868c
Michael Prather <mprather1@gmail.com>
2017-07-28 03:46:26 +0000

    update script

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 
 get_encrypted_password(){
-  /usr/local/bin/node $(directory)/run/bcrypt/index.js $1 $2 $3
+  /usr/local/bin/node $(directory)/run/bcrypt-script/index.js $1 $2 $3
 }
