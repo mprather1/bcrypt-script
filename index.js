36129d7216b28db8a574d5fbbdd8c8f13b01125b
Michael Prather <mprather1@gmail.com>
2017-07-28 03:38:57 +0000

    write file

--- index.js
+++ index.js
@@ -1,6 +1,50 @@
 var bcrypt = require('bcryptjs')
+var fs = require('fs')
+var execa = require('execa')
+var util = require('util')
 
-const password = process.argv[2]
+const username = process.argv[2]
+const password = process.argv[3]
+const filename = process.argv[4]
+
 const encryptedPassword = bcrypt.hashSync(password, bcrypt.genSaltSync(10), null)
 
-console.log(encryptedPassword)
+var command = util.format('tail -n %d %s', 1, filename)
+var command2 = util.format("sed -i '${s/$/,/}'", filename) // eslint-disable-line
+
+execa.shell(command)
+.then(result => {
+  const stdout = result.stdout
+  var vanquish = stdout.length
+
+  getStat(stdout, vanquish)
+}).catch(err => {
+  throw new Error(err)
+})
+
+function getStat (stdout, vanquish) {
+  fs.stat(filename, (err, stats) => {
+    if (err) throw new Error(err)
+    getTruncate(stats, vanquish)
+  })
+}
+
+function getTruncate (stats, vanquish) {
+  fs.truncate(filename, stats.size - vanquish, (err) => {
+    if (err) throw new Error(err)
+    appendToLastLine()
+  })
+}
+
+function appendToLastLine () {
+  execa.shell(command2)
+  .then(result => {
+    const format = {
+      username: username,
+      password: encryptedPassword
+    }
+    fs.appendFile(filename, '  ' + JSON.stringify(format) + '\n]')
+  }).catch(err => {
+    throw new Error(err)
+  })
+}
--- script.sh
+++ script.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env bash
 
 get_encrypted_password(){
-  password=$(/usr/local/bin/node $(directory)/run/bcrypt/index.js $1)
-
-  printf $password  
+  /usr/local/bin/node $(directory)/run/bcrypt/index.js $1 $2 $3
 }
